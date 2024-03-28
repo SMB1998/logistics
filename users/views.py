65a81d0d7f2d6efba5a8051e956ebcd0e88b0c78
Santiago MB <santiagomnoz318@gmail.com>
2024-03-27 21:15:14 -0500

    .

--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Users, Role
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
