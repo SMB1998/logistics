6e40ea4a3b5eb45057c06b4854eebaca325064a7
Santiago MB <santiagomnoz318@gmail.com>
2024-03-27 23:16:47 -0500

    .

--- components/views.py
+++ components/views.py
@@ -6,10 +6,17 @@ from .serializers import ComponentsSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import permission_classes
 
+from rest_framework.pagination import LimitOffsetPagination
+
+class CustomLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 100
+    max_limit = 1000  # Establece el límite máximo si lo deseas
+
 @permission_classes([IsAuthenticated])
 class ComponentsListCreateView(generics.ListCreateAPIView):
     queryset = Components.objects.all()
     serializer_class = ComponentsSerializer
+    pagination_class = CustomLimitOffsetPagination  # Agrega paginación
 
 @permission_classes([IsAuthenticated])
 class ComponentsRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
