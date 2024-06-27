04b3b7bc4ed1bbf38a4deb7346b4e35f03d05bb5
Santiago MB <santiagomnoz318@gmail.com>
2024-06-26 22:58:01 -0500

    .

--- requests/models.py
+++ requests/models.py
@@ -5,11 +5,11 @@ from components.models import Components
 
 class Requests(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    users = models.ManyToManyField(Users, related_name='requests_users', blank=True)     
-    components = models.ManyToManyField(Components, related_name='requests_components', blank=True)     
+    # users = models.ManyToManyField(Users, related_name='requests_users', blank=True)     
+    # components = models.ManyToManyField(Components, related_name='requests_components', blank=True)     
     referencia = models.CharField(max_length=100, blank=True)
     nombre = models.CharField(max_length=100)
-    admin = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='admin_requests', blank=True) 
+    # admin = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='admin_requests', blank=True) 
   
 
     def __str__(self):
