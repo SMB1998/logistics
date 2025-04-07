83bcfcea689cb37efae909cb0da82ebc823f4790
SMB1998 <santiagomunoz318@gmail.com>
2025-04-07 08:13:17 -0500

    fix post delete discussion board

--- discussion_board/signals.py
+++ discussion_board/signals.py
@@ -1,8 +1,12 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import DiscussionBoard
 from .tasks import sync_discussion_board_elasticsearch
 
 @receiver(post_save, sender=DiscussionBoard)
 def discussion_board_post_save(sender, instance, created, **kwargs):
-    sync_discussion_board_elasticsearch.delay() 
+    sync_discussion_board_elasticsearch.delay()
+
+@receiver(post_delete, sender=DiscussionBoard)
+def discussion_board_post_delete(sender, instance, **kwargs):
+    sync_discussion_board_elasticsearch.delay()
